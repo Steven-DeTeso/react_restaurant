dd93a37317490aa1d22bb9cb8a05b1cb265ad851
Steven DeTeso <sadeteso@gmail.com>
2023-02-17 22:25:21 -0500

    first commit

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
 
+import { images } from '../../constants';
 import './AboutUs.css';
 
 const AboutUs = () => (
-  <div>
-    AboutUs
+  <div className='app__aboutus app__bg flex__center section__padding' id="about">
+    <div className='app__aboutus-overlay flex__center'>
+      <img src={images.G} alt="g letter" />
+    </div>
+
+    <div className='app__aboutus-content flex__center'>
+      <div className='app__aboutus-content_about'>
+        <h1 className='headtext__cormorant'>About Us</h1>
+        <img src={images.spoon} alt="spoon" className='spoon__img' />
+        <p className='p__opensans'>A fine dining establishment with a passion for good food and drink.</p>
+        <button type='button' className='custom__button'>Know More</button>
+      </div>
+
+      <div className='app__aboutus-content_knife flex-center'>
+        <img src={images.knife} alt="about_knife" />
+      </div>
+
+      <div className='app__aboutus-content_history'>
+        <h1 className='headtext__cormorant'>Our History</h1>
+        <img src={images.spoon} alt="spoon" className='spoon__img' />
+        <p className='p__opensans'>Starting off in the burbs of Burlington in 2012. It didn't take long before this rag tag band of misfits decided to venture out on their own.</p>
+        <button type='button' className='custom__button'>Know More</button>
+      </div>
+
+    </div>
   </div>
 );
 
